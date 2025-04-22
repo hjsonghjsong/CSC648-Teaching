0999db57739f720cc8a0e879be35b33b4880dc5f
Varun Narravula <varun@snare.dev>
2025-04-22 02:07:57 -0700

    feat: first commit

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "experimenting with AI-generated tests";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+
+      perSystem = {pkgs, ...}: {
+        devShells.default = pkgs.mkShell {
+          name = "ai-generated-tests-shell";
+          packages = with pkgs; [
+            go
+            golangci-lint
+          ];
+        };
+      };
+    };
+}
